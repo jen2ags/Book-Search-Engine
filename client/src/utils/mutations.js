1543304b73b1bafb9a3ed6c9b4845f40d0b08211
jennj <jennjenn93@gmail.com>
2022-07-01 20:36:30 -0500

    add save book and remove book to mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,9 +25,39 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-
+    mutation daveBook($input: savedBook!) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
 `;
 
 export const REMOVE_BOOK = gql`
-
+    mutation removeBook ($bookId: ID!) {
+        removeBook (bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
 `;
