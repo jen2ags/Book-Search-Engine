b1f3dd80182d5676ec5b6054ad96dfc99cc2e97d
jennj <jennjenn93@gmail.com>
2022-07-03 13:21:31 -0500

    fixing server.js

--- server/server.js
+++ server/server.js
@@ -6,32 +6,29 @@ const { ApolloServer } = require('apollo-server-express');
 const {authMiddleware} = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+await server.start();
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
 
-
-
-  server.applyMiddleware({ app });
-
-  app.get("*", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 
